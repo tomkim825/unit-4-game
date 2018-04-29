46c85ba5f86e0f71e9954c97bba768601ea17fad
tomkim825 <tk825@hotmail.com>
2018-04-29 16:20:17 -0700

    finished jquery. Almost final

---
+++ README.md
@@ -0,0 +1,26 @@
+### File
+
+* Starwars game
+
+### Instructions
+
+* Open the file in Chrome to start. 
+Press "Enter" if you want to skip the beginning animation intro.
+ 
+Press Enter key to begin. This will "display none" the intro animation so the width and height are affected 
+
+Click the character to pick your user
+
+click another character to fight them
+
+click attack until someone dies
+
+if you win, click another character to fight. Repeat until game over.
+
+There is a secret final boss (master luke) that is unlocked if you play as someone other than luke.
+
+Lower middle buttons will chnage the background track and image
+****
+Known bugs:
+Jquery will not take audio.pause() to stop track before playing next one. I can't figure it out. If you change locations and again before track is done, both will play simutaneously.
+****
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,253 @@
+// code to make sure everything is ready before excuting rest of code
+$(document).ready(function() {
+
 // character database
 
 var charactersInfo = [
 {
     "id": "Luke",
     "attack": 5,
-    "health": 100,
-    "userSelected": false,
-    "computerSelected": false,
-    "deadOpponent": false,
-    "selectableCharacter": true,
-    "sprite": null,
-    "profilePicture": null,
+    "health": 100
 },
 {
     "id": "Jawa",
     "attack": 8,
-    "health": 120,
-    "userSelected": false,
-    "computerSelected": false,
-    "deadOpponent": false,
-    "selectableCharacter": true,
-    "sprite": null,
-    "profilePicture": null
+    "health": 120   
 },
 {
     "id": "Stormtrooper",
     "attack": 20,
-    "health": 150,
-    "userSelected": false,
-    "computerSelected": false,
-    "deadOpponent": false,
-    "selectableCharacter": true,
-    "sprite": null,
-    "profilePicture": null
+    "health": 150
 },
 {
     "id": "Boba-Fett",
     "attack": 25,
-    "health": 180,
-    "userSelected": false,
-    "computerSelected": false,
-    "deadOpponent": false,
-    "selectableCharacter": true,
-    "sprite": null,
-    "profilePicture": null
+    "health": 180
+},
+{
+    "id": "Master-Luke",
+    "attack": 1000,
+    "health": 1000
 }
 ];
-// initialized game info
+
+// Declare global variables
 var userSelected = false;
 var computerSelected = false;
+var userAttackPower ;
+var userHealth = 100;
+var initialUserHealth = 100;
+var userHealthPercent = 100*userHealth/initialUserHealth + "%";
+var userInfo = [{}]
+var compAttackPower ;
+var compHealth = 100;
+var initialCompHealth = 100;
+var compHealthPercent = 100*compHealth/initialUserHealth + "%";
+var compInfo = [{}];
+var score = 0;
+var gameover = false;
+var audio = $(audio);
 
-var clickCharacter = $(".swcharacter").click();
-
-function selectUser() {
-    if(!userSelected){
-    $(.swcharacter).click().append();
-    userSelected = true;
+// function to initialize game and restart/undo what is done during gameplay
+function initializeGame() {
+    userSelected = false;
+    computerSelected = false;
+    score = 0;
+    gameover = false;
+    compHealthPercent = "100%";
+    $('.comp-lifeleft').css("width",compHealthPercent);
+    userHealthPercent = "100%";
+    $('.comp-lifeleft').css("width",compHealthPercent);
+    for(i=0;i>charactersInfo.length;i++) {
+        charactersInfo[i].userSelected = false;
+        charactersInfo[i].computerSelected = false;
+        charactersInfo[i].selectableCharacter = true;
+        charactersInfo[i].deadOpponent = false
     };
+    $("#comphealth, #userhealth, #compattack, #userattack").empty();
+    $('#message-display').css("color","yellow").text("Choose your character:");
+    $('#initial-characters').css("color","yellow");
+    $('#Luke').appendTo('#initial-characters');
+    $('#Jawa').appendTo('#initial-characters');
+    $('#Stormtrooper').appendTo('#initial-characters');
+    $('#Boba-Fett').appendTo('#initial-characters');
+    $('#Master-Luke').appendTo('#initial-characters');
+    $('.luke-boy').attr("src","assets/images/luke.png");
+    $('.luke-name').text("Luke SkyWalker");
+    $('#Master-Luke').css("display","none");
+
+  };
+//   resets userside healthbar
+  function updateHealthBarUserSide() {
+    userHealthPercent = 100*userHealth/initialUserHealth + "%";
+    $('.user-lifeleft').css("width",userHealthPercent);
 };
 
-selectUser();
+//   resets computerside healthbar
+function updateHealthBarCompSide() {
+    compHealthPercent = 100*compHealth/initialCompHealth + "%";
+    $('.comp-lifeleft').css("width",compHealthPercent);
+};
+
+// function to run when user character dies to set life to 0 (incase it is negative) and display to user
+function userDied() {
+    $('#userhealth').text("0");
+    $('.user-lifeleft').css("width","0");
+    $('#message-display').css("color","white").text("You lost. Press restart button to play again?");
+    gameover = true;
+};
+
+// functon for when attack button is pressed
+function attack(){
+    // checks if game is computer character is selected. If both characters are in play and not game over, it executes
+    if((computerSelected) && (!gameover)){
+        // if next hit will not kill computer, take damage off life and display
+         if (compHealth>userAttackPower){
+        compHealth -= userAttackPower;
+        $('#comphealth').text(compHealth);
+        updateHealthBarCompSide();
+        // if user can withstand counter attack, take hit
+        if(userHealth>compAttackPower){
+            userHealth -= compAttackPower;
+            $('#userhealth').text(userHealth);
+            updateHealthBarUserSide();
+            userAttackPower += userAttackPower;
+            $('#userattack').text(userAttackPower);
+            // if user cannot withstand counter attack, run user died function
+        } else if ((compAttackPower > userHealth) || (compAttackPower == userHealth)){
+            userDied();
+        };
+// if the next hit will kill computer, set health to 0 (to prevent negative) and display it
+    } else {
+        $('.comp-lifeleft').css("width","0");
+        $('#comphealth').text(0);
+        enemyIsDefeated();
+        userAttackPower += userAttackPower;
+    $('#userattack').text(userAttackPower);
+    };
+    };    
+};
+
+// if enemy is defeated, move them to the defeated enemy section and reset their health bar
+function enemyIsDefeated(){
+    $('#'+compInfo.id).appendTo('#defeated-enemy');
+    computerSelected = false;
+    userHealth = initialUserHealth;
+    $('#userhealth').text(userHealth);
+    updateHealthBarUserSide();
+    $('#message-display').css("color","red").text("Choose Another Opponent:");
+    // keep track of score
+    score ++;
+    // if all 3 enemies are dead, show personalized message. Also reveal hidden boss if user is not luke
+    if ((score == 3)&&(userInfo.id == "Jawa")){
+        $('#message-display').css("color","white").text("OO TEE DEE!! JAWA JAWA!! You may now face Master Luke!");
+        $('#Master-Luke').css("display","block");
+    }else if ((score == 3)&&(userInfo.id == "Stormtrooper")){
+        $('#message-display').css("color","white").text("TK421 Why aren't you at your post! Return immediately or... You may now face Master Luke!");
+        $('#Master-Luke').css("display","block");
+    }else if ((score == 3)&&(userInfo.id == "Boba-Fett")){
+        $('#message-display').css("color","white").text("Well done Bounty Hunter! You shall be paid double. Do you wish to capture Master Luke for the Emperor?");
+        $('#Master-Luke').css("display","block");
+    }else if ((score == 3)&&(userInfo.id == "Luke")){
+        $('#message-display').css("color","white").text("Your training is now complete. Restart to play again. Or go to Cloud City to save your friends from Darth Vader!");
+        $('.luke-boy').attr("src","assets/images/luke_master.png");
+        $('.luke-name').text("Master Luke");
+    } else if (score > 3) {
+        $('#message-display').css("color","white").text("Game is really over. Please restart game");
+    }    
+    
+}
+
+// attack button will run attack function function when clicked
+$("#attack").on("click", function() {
+    attack();
+});
+
+// reset button will run initialize function when clicked
+  $("#restart").on("click", function() {
+      initializeGame();
+  });
+
+
+// Endor button will change jumbotron background image and audio 
+$("#Endor").on("click", function() {
+    $(".jumbotron").css("background-image","url('assets/images/endor.jpg')");
+    audio = new Audio('./assets/audio/kumite.mp3');
+    audio.play();
+});
+
+// Deathstar button will change jumbotron background image and audio 
+$("#Death-Star").on("click", function() {
+    $(".jumbotron").css("background-image","url('assets/images/deathstarbck.jpg')");
+    audio = new Audio('./assets/audio/kumite.mp3');
+    audio.play();
+});
+
+  // kumite button will change jumbotron background image and audio 
+$("#Kumite").on("click", function() {
+    $(".jumbotron").css("background-image","url('assets/images/kumite.jpg')");
+    audio = new Audio('./assets/audio/kumite.mp3');
+    audio.play();
+});
+
+
+// Cantina button will change jumbotron background image and audio 
+$("#Cantina").on("click", function() {
+    $(".jumbotron").css("background-image","url('assets/images/cantina.jpg')");
+    audio = new Audio('./assets/audio/cantina.mp3');
+    audio.play();
+});
+
+    // Enter keypress to start game, clear intro animation divs, show characters 
+$("body").keypress(function (e) {
+    if (event.key == "Enter"){
+    $(".introcontainer").css("display","none");
+    $(".press-enter").css("display","none");
+    $(".swcharacter").css("display","block");
+    $('#Master-Luke').css("display","none");
+    }
+  });
+
+    // Add an on click listener to all elements that have the class "swcharacter". This is to select characters
+    $(".swcharacter").on("click", function() {
+
+    // Check if user has selected character. if selected, checks if computer/opponent is selected
+    if (!userSelected){
+        $(this).appendTo("#usercharacter");
+        userSelected = true;
+        // runs a loop to search for the corresponding object with info, then assigns values
+        for(i=0; i<charactersInfo.length; i++){
+            if(this.id == charactersInfo[i].id){
+                userInfo = charactersInfo[i];   
+            };
+        userAttackPower = userInfo.attack;
+        userHealth = userInfo.health;
+        initialUserHealth = userInfo.health;
+        $('#userhealth').text(userHealth);
+        $('#userattack').text(userAttackPower);
+        updateHealthBarUserSide();
+        };
+        // once user is selected, opponent selection begins
+        $('#message-display').css("color","red").text("Now choose your opponent:");
+        $('#initial-characters').css("color","red");
+    } else if(!computerSelected){
+        $(this).appendTo("#computercharacter");
+        computerSelected = true;
+        // runs a loop to search for the corresponding object with info, then assigns values
+        for(i=0; i<charactersInfo.length; i++){
+            if(this.id == charactersInfo[i].id){
+                compInfo = charactersInfo[i];   
+            };
+        compAttackPower = compInfo.attack;
+        compHealth = compInfo.health;
+        initialCompHealth = compInfo.health;
+        $('#comphealth').text(compHealth);
+        $('#compattack').text(compAttackPower);
+        updateHealthBarCompSide();
+        };
+    };
+
+
+    });
+initializeGame();
+});
